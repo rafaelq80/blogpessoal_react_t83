b3500de4ef7f590349daf380ec51f692fd240dd3
Rafael Queiroz <rafaelproinfo@gmail.com>
2025-11-13 12:43:49 -0300

    :sparkles: Construção dos Componentes Home, Navbar e Footer. Adição das primeiras Rotas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Footer from "./components/footer/Footer"
+import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
-
+import Login from "./pages/login/Login"
+import Cadastro from "./pages/cadastro/Cadastro"
 
 function App() {
-
-  
 	return (
 		<>
-      <Home />
+			<BrowserRouter>
+				<Navbar />
+				<div className="min-h-[80vh]">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/home" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/cadastro" element={<Cadastro />} />
+					</Routes>
+				</div>
+				<Footer />
+			</BrowserRouter>
 		</>
 	)
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,78 +5,41 @@ function Home() {
   return (
     /* Container */
     <div
-        style={{
-            backgroundColor: "#312e81",
-            display: "flex",
-            justifyContent: "center"
-        }}
+        className="bg-indigo-900 flex justify-center"
     >
         {/* Grid que divide a tela em 2 colunas */}
         <div
-            style={{
-                display: "grid",
-                gridTemplateColumns: "1fr 1fr",
-                color: "white",
-                width: "100%",
-                maxWidth: "1280px"
-            }}
+            className="container grid grid-cols-1 sm:grid-cols-2 text-white"
         >
             {/* Coluna esquerda */}
             <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "1rem",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    paddingTop: "1rem",
-                    paddingBottom: "1rem"
-                }}
+                className="flex flex-col gap-4 items-center justify-center py-4"
             >
                 <h2
-                    style={{
-                        fontSize: "3rem",
-                        fontWeight: "bold"
-                    }}
+                    className="text-5xl font-bold"
                 >Seja Bem Vinde!</h2>
                 <p
-                    style={{
-                        fontSize: "1.25rem"
-                    }}
+                    className="text-xl"
                 >Expresse aqui seus pensamentos e opiniões</p>
                 
                 {/* Link/Botão */}
                 <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "space-around",
-                        gap: "1rem"
-                    }}  
+                    className="flex justify-around gap-4"
                 >
                     <div
-                        style={{
-                            borderRadius: "0.5rem",
-                            color: "white",
-                            border: "2px solid white",
-                            padding: "0.5rem 1rem"
-                        }}
+                        className="rounded text-white border-white border-solid border-2 py-2 px-4"
                     >Nova Postagem</div>
                 </div>
             </div>
 
             {/* Coluna Direita */}
             <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center"
-                }}
+                className="flex justify-center"
             >
                 <img
                     src="https://i.imgur.com/fyfri1v.png"
                     alt="Imagem da Página Home"
-                    style={{
-                        width: "66%"
-                    }}
+                    className="w-2/3"
                 />
             </div>
         </div>
