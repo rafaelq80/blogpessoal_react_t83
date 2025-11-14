d4eb0e2ea9edf54843a324655981add064275077
Rafael Queiroz <rafaelproinfo@gmail.com>
2025-11-14 17:43:07 -0300

    :sparkles: Construção da Página de Login

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,19 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+	const navigate = useNavigate();
+
+	const { handleLogout } = useContext(AuthContext);
+
+	function logout(){
+		handleLogout();
+		alert('O Usuário foi desconectado com sucesso!');
+		navigate('/');
+	}
+
 	return (
 		<>
 			<div
@@ -17,7 +30,7 @@ function Navbar() {
                         Temas 
                         Cadastrar tema 
                         Perfil 
-                        Sair
+                        <Link to='' onClick={logout} className="hover:underline">Sair</Link>
                     </div>
 				</div>
 			</div>
